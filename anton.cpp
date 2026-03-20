d53a7b9e1820716c334438b3ddf81399300033bb
Manjyotwadhwa <2023.manjyotsingh.wadhwa@ves.ac.in>
2026-03-20 14:26:03 +0530

    Add files via upload

---
+++ anton.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<string>
+using namespace std ;
+
+int main(){
+int n ;
+cin>>n;
+string s ;
+cin>>s;
+int countA = 0;
+int countD = 0;
+for(int i = 0; i<s.length();i++){
+   if(s[i] =='A'){
+    countA++;
+   }
+   else {
+    countD++;
+   }
+}
+if(countA>countD){
+    cout<<"Anton"<<endl;
+}
+else if (countD>countA){
+    cout<<"Danik"<<endl;
+}
+else {
+    cout<<"Friendship"<<endl;
+}
+  return 0;
+}
