eba44a3ebaa951fae9b1404d6aecffd711699f8e
Manjyotwadhwa <2023.manjyotsingh.wadhwa@ves.ac.in>
2026-03-22 00:04:51 +0530

    Add files via upload

---
+++ GoodArrays.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+  int t ;
+  cin>>t;
+  while(t--){
+    int n ;
+    cin>>n;
+    vector<int>a(n);
+
+
+    for(int i = 0; i<n;i++){
+        cin>>a[i];
+    }
+      int count = 0;
+    for(int i = 0; i<n-1;i++){
+    if((a[i]%2 ==0 && a[i+1]%2 == 0) || (a[i]%2 !=0 && a[i+1]%2 != 0)){
+        count++;
+    }
+}
+cout<<count<<endl;
+
+  }
+   
+  return 0;
+}
